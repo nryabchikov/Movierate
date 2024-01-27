cd16772dd5823456ed8390cfb580651b4aaf5473
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-01-27 17:08:42 +0300

    feat: my exception to validate data

---
+++ src/main/java/by/nekitos/group/movierate/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package by.nekitos.group.movierate.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String str) {
+        super(str);
+    }
+}
