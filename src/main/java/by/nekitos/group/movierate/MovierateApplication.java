b51c49dd261cefadaf8eb30dcbcad35e717bcf52
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-01-27 17:02:29 +0300

    feat: project initializr

---
+++ src/main/java/by/nekitos/group/movierate/MovierateApplication.java
@@ -0,0 +1,13 @@
+package by.nekitos.group.movierate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/nekitos/group/movierate/MovierateApplicationTests.java
@@ -0,0 +1,13 @@
+package by.nekitos.group.movierate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
