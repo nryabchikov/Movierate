ba794c48ec704d89eaa997a99587904b9c01255e
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-01-27 20:36:13 +0300

    feat: test to film validation

---
+++ src/test/java/by/nekitos/group/movierate/storage/FilmStorageTest.java
@@ -0,0 +1,79 @@
+package by.nekitos.group.movierate.storage;
+
+import by.nekitos.group.movierate.exception.ValidationException;
+import by.nekitos.group.movierate.model.Film;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmStorageTest {
+
+    private FilmStorage filmStorage;
+    private Film film;
+
+    @BeforeEach
+    public void setUpFilmStorage() {
+        filmStorage = new FilmStorage();
+    }
+
+    @BeforeEach
+    public void setUpFilm() {
+        film = new Film();
+    }
+
+    @Test
+    public void testCreateFilm() {
+        film.setName("Test Film");
+        film.setDescription("This is a test film.");
+        film.setReleaseDate(LocalDate.now());
+        film.setDuration(120);
+        Film createdFilm = filmStorage.create(film);
+        assertEquals(film, createdFilm);
+        assertTrue(filmStorage.output().contains(film));
+    }
+
+    @Test
+    public void testCreateFilmWithInvalidName() {
+        film.setName("");
+        film.setDescription("This is a test film.");
+        film.setReleaseDate(LocalDate.now());
+        film.setDuration(120);
+        assertThrows(ValidationException.class, () -> filmStorage.create(film));
+    }
+
+    @Test
+    public void testCreateFilmWithInvalidDescription() {
+        film.setName("Test Film");
+        film.setReleaseDate(LocalDate.now());
+        film.setDuration(120);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 200; i++) {
+            sb.append(i);
+        }
+        film.setDescription(sb.toString());
+        assertThrows(ValidationException.class, () -> filmStorage.create(film));
+    }
+
+    @Test
+    public void testCreateFilmWithInvalidDate() {
+        film.setName("Test Film");
+        film.setDescription("This is a test film.");
+        LocalDate localDate = LocalDate.of(1985, 12, 27);
+        film.setReleaseDate(localDate);
+        film.setDuration(120);
+
+        assertThrows(ValidationException.class, () -> filmStorage.create(film));
+    }
+
+    @Test
+    public void testCreateFilmWithInvalidDuration() {
+        film.setName("Test Film");
+        film.setDescription("This is a test film.");
+        film.setReleaseDate(LocalDate.now());
+        film.setDuration(-1);
+        assertThrows(ValidationException.class, () -> filmStorage.create(film));
+    }
+}
